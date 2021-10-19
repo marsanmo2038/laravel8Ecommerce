c52811b23c8f66ad7c2015026f1f2a25d9124717
Marcos Sanchez <marsanmo2038@gmail.com>
2021-10-20 01:08:18 +0200

    007 add cart

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
+use Cart;
 use Livewire\Component;
 
 class DetailsComponent extends Component
@@ -29,9 +30,9 @@ class DetailsComponent extends Component
 
     public function store($product_id, $product_name, $product_price)
     {
-      /*   Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
         session()->flash('success_message', 'Item added in Cart');
 
-        return redirect()->route('product.cart'); */
+        return redirect()->route('product.cart'); 
     }
 }
--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -3,9 +3,10 @@
 namespace App\Http\Livewire;
 
 use App\Models\Product;
-use App\Models\User;
 use Livewire\Component;
 use Livewire\WithPagination;
+use App\Models\Category;
+use Cart;
 
 class ShopComponent extends Component
 {
@@ -23,27 +24,35 @@ class ShopComponent extends Component
     public function render()
     {
        
-        $products = Product::all();
+        if($this->sorting == 'date'){
+            $products = Product::orderBy('created_at', 'DESC')->paginate($this->pagesize);
+        }
+        else if($this->sorting == 'price'){
+            $products = Product::orderBy('regular_price', 'ASC')->paginate($this->pagesize);
+        }
+        else if($this->sorting == 'price-desc'){
+            $products = Product::orderBy('regular_price', 'DESC')->paginate($this->pagesize);
+        }
+        else{
+            $products = Product::paginate($this->pagesize);
+        }
+        
+        $popular_products = Product::inRandomOrder()->limit(4)->get();
 
-       // dd($products);
-
-        $popular_products = collect([
-            ['product_id' => 'prod-100', 'name' => 'Desk'],
-            ['product_id' => 'prod-200', 'name' => 'Chair'],
-        ]);
-        $categories = collect([
-            ['product_id' => 'prod-100', 'name' => 'Desk'],
-            ['product_id' => 'prod-200', 'name' => 'Chair'],
-        ]);
+        $categories = Category::all();
 
         return view('livewire.shop-component', [
-            'productss' => $products
-           
+            'productss' => $products,
+            'popular_products' => $popular_products,
+            'categories' => $categories
         ])->layout('layouts.base');
     }
 
     public function store($product_id, $product_name, $product_price)
     {
-       
+        Cart::add($product_id, $product_name, 1, $product_price)->associate('App\Models\Product');
+        session()->flash('success_message', 'Item added in Cart');
+
+        return redirect()->route('product.cart');
     }
 }
