6b72b8ab9f7c8fcb17574ec7ec5770e6d807198e
Marcos Sanchez <marsanmo2038@gmail.com>
2021-10-22 13:05:28 +0200

    017 lista de productos

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Category as ModelsCategory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
+
+
 }
