c28ea4e8a91a40765ce4aee636d0d6dce8f13a09
Marcos Sanchez <marsanmo2038@gmail.com>
2021-11-29 11:55:40 +0100

    Show MY Orders 46

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,8 @@ use App\Http\Livewire\SearchComponent;
 use App\Http\Livewire\ShopComponent;
 use App\Http\Livewire\ThankyouComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
+use App\Http\Livewire\User\UserOrderDetailsComponent;
+use App\Http\Livewire\User\UserOrdersComponent;
 use App\Http\Livewire\WishlistComponent;
 use Illuminate\Support\Facades\Route;
 
@@ -70,6 +72,9 @@ Route::get('/thank-you',ThankyouComponent::class)->name('thankyou');
 // Dashboard particular para usuarios o clientes
 Route::middleware(['auth:sanctum', 'verified'])->group(function(){
     Route::get('/user/dashboard', UserDashboardComponent::class)->name('user.dashboard');
+    Route::get('/user/orders', UserOrdersComponent::class)->name('user.orders');
+    Route::get('/user/orders/{order_id}', UserOrderDetailsComponent::class)->name('user.orderdetails');
+
 });
 
 // Dashboard particular para administradores ,'authadmin'
